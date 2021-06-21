0fe9af30a1e6b429cee9921d00224c800e0f3c49
weisseln <77675839+weisseln@users.noreply.github.com>
2021-06-21 15:23:49 +0200

    new inserts

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -9,8 +9,14 @@ public class MainAppl {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("== " + player);
 		
-		Player player = new Player(4, "Hinteregger", "Defense");
+		Player player = new Player(5, "Hinteregger", "Defense");
 		System.out.println("== " + player);
+		
+		Player player = new Player(6, "Mavkovic", "Defense");
+		System.out.println("== " + player);
+		
+		Player player5 = new Player(8, "Mercedes", "Attack");
+		System.out.println("== " + player5);
 		
 		
 		
