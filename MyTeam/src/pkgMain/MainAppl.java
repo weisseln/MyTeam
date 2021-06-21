3f8eaa04e4ea57517d2b9c05fbf20bbe9306939e
Noah Tobias Weissel <ntmcool9@gmail.com>
2021-06-21 15:24:58 +0200

    New Players and changed toString

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -9,9 +9,11 @@ public class MainAppl {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("== " + player);
 		
-		Player player = new Player(4, "Hinteregger", "Defense");
-		System.out.println("== " + player);
+		Player player2 = new Player(4, "Hinteregger", "Defense");
+		System.out.println("== " + player2);
 		
+		Player player3 = new Player(4, "Berger", "Hinterweltler");
+		System.out.println("== " + player3);
 		
 		
 		
